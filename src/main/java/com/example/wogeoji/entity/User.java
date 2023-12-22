2ecb6c64ed90b83516634ffe4f227894fbf8c8ac
hyeg0121 <w2218@e-mirim.hs.kr>
2023-12-22 20:04:09 +0900

    feat: 유저 엔티티 추가

---
+++ src/main/java/com/example/wogeoji/entity/User.java
@@ -0,0 +1,35 @@
+package com.example.wogeoji.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_no", updatable = false)
+    private Long no;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true)
+    private String id;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    @ColumnDefault("'아직 상태메세지가 없습니다.'")
+    private String bio;
+}
