89ced83d841402347bb08d059c9c5d20dd4dec32
hyeg0121 <w2218@e-mirim.hs.kr>
2023-12-22 13:39:32 +0900

    initial commit

---
+++ src/main/java/com/example/wogeoji/WogeojiApplication.java
@@ -0,0 +1,13 @@
+package com.example.wogeoji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WogeojiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WogeojiApplication.class, args);
+    }
+
+}
