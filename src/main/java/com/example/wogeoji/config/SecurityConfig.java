b57818b3e850125d7b83532ccabfddafa1b60faf
hyeg0121 <w2218@e-mirim.hs.kr>
2023-12-23 18:01:19 +0900

    feat: 시큐리티 설정

---
+++ src/main/java/com/example/wogeoji/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.wogeoji.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/**").permitAll();
+            auth.anyRequest().authenticated();
+        })
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
