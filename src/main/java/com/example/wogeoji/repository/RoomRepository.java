6c7829a9c29bacab20b3528dca47865a4b9dc30f
hyeg0121 <w2218@e-mirim.hs.kr>
2023-12-23 23:41:30 +0900

    feat: 거지방 리포지토리 추가

---
+++ src/main/java/com/example/wogeoji/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.example.wogeoji.repository;
+
+import com.example.wogeoji.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
