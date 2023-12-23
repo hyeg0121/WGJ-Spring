938fb01a129b52d7fa3e08f573367c07fa118a3f
hyeg0121 <w2218@e-mirim.hs.kr>
2023-12-23 18:15:32 +0900

    feat: 유저 서비스 추가

---
+++ src/main/java/com/example/wogeoji/service/UserService.java
@@ -0,0 +1,56 @@
+package com.example.wogeoji.service;
+
+import com.example.wogeoji.entity.User;
+import com.example.wogeoji.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // 모든 유저 조회
+    public List<User> findAllUser() {
+        return userRepository.findAll();
+    }
+
+    // pk로 유저 조회
+    public Optional<User> findUserByNo(Long no) {
+        return userRepository.findById(no);
+    }
+
+    // 유저 생성
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    // 유저 업데이트
+    public User updateUser(Long userNo, User updateUser) {
+        Optional<User> existingUserOptional = userRepository.findById(userNo);
+
+        if (existingUserOptional.isPresent()) {
+            User existingUser = existingUserOptional.get();
+            existingUser.setName(updateUser.getName());
+            existingUser.setBio(updateUser.getBio());
+            existingUser.setPassword(updateUser.getPassword());
+
+            return userRepository.save(existingUser);
+        }
+
+        return null;
+    }
+
+    // 유저 삭제
+    public void deleteUser(Long userNo) {
+        userRepository.deleteById(userNo);
+    }
+}
