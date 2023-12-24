6c8449f458ace1b1bcbafb67ca743183d1ad4387
hyeg0121 <w2218@e-mirim.hs.kr>
2023-12-24 17:24:08 +0900

    feat: 거지방 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO users (name, id, password, email, bio) VALUES ('황혜경', 'hye_123', 'password123', 'hye@example.com', '안녕!');
 INSERT INTO users (name, id, password, email, bio) VALUES ('손아섭', 'son_123', 'password123', 'son@example.com', '야구가 좋아!');
+
+INSERT INTO rooms (name, creator_no, description, capacity) VALUES
+                                                                ('커피 아끼기', 1, '커피를 덜 마셔서 거지를 탈출하자', 10),
+                                                                ('야빠 거지방', 2, '거지 두산베이스 방', 15);
