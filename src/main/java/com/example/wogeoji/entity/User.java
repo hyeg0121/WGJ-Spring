5cb00e17adb9f2047e57939b54968ed2d75a7383
hyeg0121 <w2218@e-mirim.hs.kr>
2023-12-24 22:50:38 +0900

    feat: 유저 엔티티 지출 금액, 가입 날짜 추가

--- src/main/java/com/example/wogeoji/entity/User.java
+++ src/main/java/com/example/wogeoji/entity/User.java
@@ -1,17 +1,19 @@
 package com.example.wogeoji.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,4 +33,13 @@ public class User {
     private String email;
 
     private String bio;
+
+    @Column(name = "spent_amount")
+    private Integer spentAmount;
+
+    @CreatedDate
+    @Column(name = "join_date")
+    private LocalDateTime joinDate;
+
+
 }
