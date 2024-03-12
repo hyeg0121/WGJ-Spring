f8b5b9d82b3f6325bd6aefa798dab4bbfb52433e
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-12 20:44:45 +0900

    feat: 댓글 엔티티 추가

---
+++ src/main/java/com/example/wogeoji/entity/Comment.java
@@ -0,0 +1,28 @@
+package com.example.wogeoji.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column(nullable = false)
+    private String content;
+}
