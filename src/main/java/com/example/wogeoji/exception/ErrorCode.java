2c87bea78d816d1b8974aecf802000ee3ca8d1e9
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-13 22:05:27 +0900

    feat: IncorrectPasswordException 추가

--- src/main/java/com/example/wogeoji/exception/ErrorCode.java
+++ src/main/java/com/example/wogeoji/exception/ErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    DUPLICATE_EMAIL(HttpStatus.CONFLICT, "해당 이메일을 사용하는 유저가 존재합니다.");
+    DUPLICATE_EMAIL(HttpStatus.CONFLICT, "해당 이메일을 사용하는 유저가 존재합니다."),
+    INCORRECT_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 알맞지 않습니다.");
 
 
     private final HttpStatus httpStatus;
