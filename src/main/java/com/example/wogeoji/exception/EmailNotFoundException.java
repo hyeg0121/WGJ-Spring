2995a50cc41f5f3a3b1b3db9407e7c35d74272de
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-15 19:00:05 +0900

    move: 디렉토리 이동 (user 패키지 삭제)

--- src/main/java/com/example/wogeoji/exception/EmailNotFoundException.java
+++ src/main/java/com/example/wogeoji/exception/EmailNotFoundException.java
@@ -1,7 +1,4 @@
-package com.example.wogeoji.exception.user;
-
-import com.example.wogeoji.exception.BusinessException;
-import com.example.wogeoji.exception.ErrorCode;
+package com.example.wogeoji.exception;
 
 public class EmailNotFoundException extends BusinessException {
     public static final BusinessException EXCEPTION = new EmailNotFoundException();
