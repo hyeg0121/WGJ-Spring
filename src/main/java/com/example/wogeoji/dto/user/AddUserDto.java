b0ba326e2e4eb86184e79dc4ee58bafa63c8fff4
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-15 18:10:10 +0900

    feat: Setter 추가

--- src/main/java/com/example/wogeoji/dto/user/AddUserDto.java
+++ src/main/java/com/example/wogeoji/dto/user/AddUserDto.java
@@ -1,9 +1,10 @@
 package com.example.wogeoji.dto.user;
 
-import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class AddUserDto {
     private String name;
     private String email;
