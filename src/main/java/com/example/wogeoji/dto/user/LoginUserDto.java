7707d4ad9273b8fe4fb2693125b7a3238162c237
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-24 17:54:24 +0900

    feat: LoginUserDto 추가

---
+++ src/main/java/com/example/wogeoji/dto/user/LoginUserDto.java
@@ -0,0 +1,11 @@
+package com.example.wogeoji.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginUserDto {
+    private String email;
+    private String password;
+}
