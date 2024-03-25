01fa5f0dacb7ddd72f5ddd11181f0c9c1f041ec9
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-25 11:52:55 +0900

    feat: RoomResponse 적용

--- src/main/java/com/example/wogeoji/controller/RoomController.java
+++ src/main/java/com/example/wogeoji/controller/RoomController.java
@@ -1,6 +1,7 @@
 package com.example.wogeoji.controller;
 
 import com.example.wogeoji.dto.room.AddRoomDto;
+import com.example.wogeoji.dto.room.RoomResponseDto;
 import com.example.wogeoji.entity.Room;
 import com.example.wogeoji.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,23 +24,24 @@ public class RoomController {
 
     // 모든 거지방 조회
     @GetMapping
-    public List<Room> getAllRooms() {
-        return roomService.findAllRooms();
+    public ResponseEntity<List<RoomResponseDto>> getAllRooms() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(roomService.findAllRooms());
     }
 
     // pk로 거지방 조회
     @GetMapping("/{roomId}")
-    public ResponseEntity<Room> getRoomById(@PathVariable Long roomId) {
-        Optional<Room> room = roomService.findRoomById(roomId);
-        return room.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-
+    public ResponseEntity<RoomResponseDto> getRoomById(@PathVariable Long roomId) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(roomService.findRoomById(roomId));
     }
 
     // 거지방 추가
     @PostMapping
-    public ResponseEntity<Room> addRoom(@RequestBody AddRoomDto addRoomDto) {
-        Room createdRoom = roomService.addRoom(addRoomDto);
-        return new ResponseEntity<>(createdRoom, HttpStatus.CREATED);
+    public ResponseEntity<RoomResponseDto> addRoom(@RequestBody AddRoomDto addRoomDto) {
+        RoomResponseDto createdRoom = roomService.addRoom(addRoomDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(createdRoom);
     }
 }
--- src/main/java/com/example/wogeoji/service/RoomService.java
+++ src/main/java/com/example/wogeoji/service/RoomService.java
@@ -1,8 +1,10 @@
 package com.example.wogeoji.service;
 
 import com.example.wogeoji.dto.room.AddRoomDto;
+import com.example.wogeoji.dto.room.RoomResponseDto;
 import com.example.wogeoji.entity.Room;
 import com.example.wogeoji.entity.User;
+import com.example.wogeoji.exception.RoomNotFoundException;
 import com.example.wogeoji.exception.UserNotFoundException;
 import com.example.wogeoji.repository.RoomRepository;
 import com.example.wogeoji.repository.UserRepository;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class RoomService {
@@ -25,28 +28,34 @@ public class RoomService {
     }
 
     // 모든 거지방 조회
-    public List<Room> findAllRooms() {
-        return roomRepository.findAll();
+    public List<RoomResponseDto> findAllRooms() {
+        return roomRepository.findAll()
+                .stream()
+                .map(RoomResponseDto::from)
+                .collect(Collectors.toList());
     }
 
     // pk로 거지방 조회
-    public Optional<Room> findRoomById(Long roomId) {
-        return roomRepository.findById(roomId);
+    public RoomResponseDto findRoomById(Long roomId) {
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(() -> RoomNotFoundException.EXCEPTION);
+
+        return RoomResponseDto.from(room);
     }
 
     // 거지방 생성
-    public Room addRoom(AddRoomDto addRoomDto) {
+    public RoomResponseDto addRoom(AddRoomDto addRoomDto) {
         User creator = userRepository.findById(addRoomDto.getCreatorId())
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
 
-
-        Room newRoom = Room.builder()
+        Room savedRoom = roomRepository.save(Room.builder()
                 .name(addRoomDto.getName())
                 .capacity(addRoomDto.getCapacity())
                 .creator(creator)
                 .description(addRoomDto.getDescription())
-                .build();
+                .build());
 
-        return roomRepository.save(newRoom);
+
+        return RoomResponseDto.from(savedRoom);
     }
 }
