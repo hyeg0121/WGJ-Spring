0055bf541e03909c56f3b7fe2d106cdd85f9f791
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-06 18:41:17 +0900

    feat: 거지방 만들면 자동으로 가입

--- src/main/java/com/example/wogeoji/service/RoomService.java
+++ src/main/java/com/example/wogeoji/service/RoomService.java
@@ -4,15 +4,16 @@ import com.example.wogeoji.dto.room.AddRoomDto;
 import com.example.wogeoji.dto.room.RoomResponseDto;
 import com.example.wogeoji.entity.Room;
 import com.example.wogeoji.entity.User;
+import com.example.wogeoji.entity.UserRoom;
 import com.example.wogeoji.exception.RoomNotFoundException;
 import com.example.wogeoji.exception.UserNotFoundException;
 import com.example.wogeoji.repository.RoomRepository;
 import com.example.wogeoji.repository.UserRepository;
+import com.example.wogeoji.repository.UserRoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,11 +21,13 @@ public class RoomService {
 
     private final RoomRepository roomRepository;
     private final UserRepository userRepository;
+    private final UserRoomRepository userRoomRepository;
 
     @Autowired
-    public RoomService(RoomRepository roomRepository, UserRepository userRepository) {
+    public RoomService(RoomRepository roomRepository, UserRepository userRepository, UserRoomRepository userRoomRepository) {
         this.roomRepository = roomRepository;
         this.userRepository = userRepository;
+        this.userRoomRepository = userRoomRepository;
     }
 
     // 모든 거지방 조회
@@ -48,6 +51,7 @@ public class RoomService {
         User creator = userRepository.findById(addRoomDto.getCreatorId())
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
 
+
         Room savedRoom = roomRepository.save(Room.builder()
                 .name(addRoomDto.getName())
                 .capacity(addRoomDto.getCapacity())
@@ -55,7 +59,18 @@ public class RoomService {
                 .description(addRoomDto.getDescription())
                 .build());
 
+        joinRoom(savedRoom);
+
 
         return RoomResponseDto.from(savedRoom);
     }
+
+    // 가입하기
+    public UserRoom joinRoom(Room room) {
+        return userRoomRepository.save(UserRoom.builder()
+                .user(room.getCreator())
+                .room(room)
+                .build());
+    }
+
 }
