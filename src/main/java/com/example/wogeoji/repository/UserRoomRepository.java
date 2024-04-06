2cf23ed0a81d7f0fd718a908a4097a93b3a21e3f
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-06 18:41:51 +0900

    feat: 유저 아이디로 가입된 방 찾는 쿼리 추가

--- src/main/java/com/example/wogeoji/repository/UserRoomRepository.java
+++ src/main/java/com/example/wogeoji/repository/UserRoomRepository.java
@@ -1,9 +1,16 @@
 package com.example.wogeoji.repository;
 
+import com.example.wogeoji.entity.Room;
 import com.example.wogeoji.entity.UserRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRoomRepository extends JpaRepository<UserRoom, Long> {
+    @Query("SELECT ur.room FROM UserRoom ur WHERE ur.user.id = :userId")
+    List<Room> findRoomsByUserId(@Param("userId") Long userId);
 }
