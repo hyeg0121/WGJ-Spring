1e0929c79caa98276b3ef448ef7b25f6bc1fed02
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-08 20:05:36 +0900

    refactor: creator 정보 제거

--- src/main/java/com/example/wogeoji/dto/room/RoomResponseDto.java
+++ src/main/java/com/example/wogeoji/dto/room/RoomResponseDto.java
@@ -13,29 +13,25 @@ public class RoomResponseDto extends BasicResponseDto {
 
     private final Long id;
     private final String name;
-    private final UserResponseDto creator;
     private final String description;
     private final Integer capacity;
 
     @Builder
-    public RoomResponseDto(Integer isDeleted, LocalDateTime createdAt, LocalDateTime updatedAt, Long id, String name, UserResponseDto creator, String description, Integer capacity) {
+    public RoomResponseDto(Integer isDeleted, LocalDateTime createdAt, LocalDateTime updatedAt, Long id, String name, String description, Integer capacity) {
         super(isDeleted, createdAt, updatedAt);
         this.id = id;
         this.name = name;
-        this.creator = creator;
         this.description = description;
         this.capacity = capacity;
     }
 
     public static RoomResponseDto from(Room room) {
-        UserResponseDto creatorDto = UserResponseDto.from(room.getCreator());
         return RoomResponseDto.builder()
                 .isDeleted(room.getIsDeleted())
                 .createdAt(room.getCreatedAt())
                 .updatedAt(room.getUpdatedAt())
                 .id(room.getId())
                 .name(room.getName())
-                .creator(creatorDto)
                 .description(room.getDescription())
                 .capacity(room.getCapacity())
                 .build();
